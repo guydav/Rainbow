402ae7b5d18ab837b99625de87c6bf0c881febdf
Guy Davidson <guydav@gmail.com>
2019-09-20 14:12:46 -0400

    Added explicit converstion to uint8 to avoid imageio warnings.

--- test.py
+++ test.py
@@ -44,7 +44,8 @@ def test(args, T, dqn, val_mem, metrics, results_dir, evaluate=False):
       if done:
         T_rewards.append(reward_sum)
         if args.evaluation_gifs:
-          imageio.mimwrite(os.path.join(results_dir, 'eval_gifs', f'eval-{T}-{i}.gif'), gif_stack, fps=10)
+          imageio.mimwrite(os.path.join(results_dir, 'eval_gifs', f'eval-{T}-{i}.gif'),
+                           [(frame * 255).astype(np.uint8) for frame in gif_stack], fps=10)
         break
   env.close()
 
