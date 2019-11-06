c372752e93475efb64a1d6b6d9fbad997375b7a1
Guy Davidson <guydav@gmail.com>
2019-11-06 18:59:01 -0500

    Minor bugfix

--- model.py
+++ model.py
@@ -51,17 +51,15 @@ class DQN(nn.Module):
     super(DQN, self).__init__()
     self.atoms = args.atoms
     self.action_space = action_space
-    state_depth = args.state_depth
-
     if args.architecture == 'canonical':
       self.convs = nn.Sequential(nn.Conv2d(args.history_length * args.state_depth, 32, 8, stride=4, padding=0), nn.ReLU(),
                                  nn.Conv2d(32, 64, 4, stride=2, padding=0), nn.ReLU(),
                                  nn.Conv2d(64, 64, 3, stride=1, padding=0), nn.ReLU())
-      self.conv_output_size = 3136 * state_depth
+      self.conv_output_size = 3136
     elif args.architecture == 'data-efficient':
       self.convs = nn.Sequential(nn.Conv2d(args.history_length, 32, 5, stride=5, padding=0), nn.ReLU(),
                                  nn.Conv2d(32, 64, 5, stride=5, padding=0), nn.ReLU())
-      self.conv_output_size = 576 * state_depth
+      self.conv_output_size = 576
     self.fc_h_v = NoisyLinear(self.conv_output_size, args.hidden_size, std_init=args.noisy_std)
     self.fc_h_a = NoisyLinear(self.conv_output_size, args.hidden_size, std_init=args.noisy_std)
     self.fc_z_v = NoisyLinear(args.hidden_size, self.atoms, std_init=args.noisy_std)
