add3b07a9cce52f43fdda4c8ff79de06b05dc5eb
Guy Davidson <guydav@gmail.com>
2020-01-13 16:28:55 +0200

    Slightly improved the state-saving logic to make it easier to use.

--- test.py
+++ test.py
@@ -63,12 +63,12 @@ def test(args, T, dqn, val_mem, metrics, results_dir, evaluate=False):
       if done:
         state, reward_sum, done = env.reset(), 0, False
         if args.save_evaluation_gifs:
-          gif_stack.append(env.full_observation_buffer[-1].cpu().numpy())
+          gif_stack.extend([x.cpu().numpy() for x in env.full_observation_buffer])
 
         if args.save_evaluation_states:
           grayscale_states.append(np.moveaxis(env.ale.getScreenGrayscale(), 2, 0))
           color_states.append(np.expand_dims(env.ale.getScreenRGB(), 0))
-          env_states.append(torch.stack(list(env.full_observation_buffer), 0).cpu().numpy())
+          env_states.extend([x.cpu().numpy() for x in env.full_observation_buffer])
 
       action = dqn.act_e_greedy(state)  # Choose an action ε-greedily
       state, reward, done = env.step(action)  # Step
@@ -79,7 +79,7 @@ def test(args, T, dqn, val_mem, metrics, results_dir, evaluate=False):
       if args.save_evaluation_states:
         grayscale_states.append(np.moveaxis(env.ale.getScreenGrayscale(), 2, 0))
         color_states.append(np.expand_dims(env.ale.getScreenRGB(), 0))
-        env_states.append(torch.stack(list(env.full_observation_buffer), 0).cpu().numpy())
+        env_states.append(env.full_observation_buffer[-1].cpu().numpy())
 
       reward_sum += reward
       if args.render:
